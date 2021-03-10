65d0010474b10fe77cd6ba0c46ce9ba0f189a3eb
YD <yahordunayeu@gmail.com>
2021-03-10 19:29:47 +0100

    start work with Redux / add styles and interactive

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import {createStore} from 'redux';
+
+
+const reducer = (state = 0, action) => {
+    switch (action.type) {
+        case 'INC':
+            return state + 1;
+        case 'DEC':
+            return state - 1;
+        case 'RES':
+            return state = 0;
+        default:
+            return state;
+    }
+}
+
+const inc = () => ({type: 'INC'});
+const dec = () => ({type: 'DEC'});
+const res = (value) => ({type: 'RES', value});
+
+const store = createStore(reducer);
+
+document.getElementById('inc').addEventListener('click', () => {
+    store.dispatch(inc())
+});
+document.getElementById('dec').addEventListener('click', () => {
+    store.dispatch(dec())
+});
+document.getElementById('res').addEventListener('click', () => {
+    const value = 0;
+    store.dispatch(res(value))
+});
+
+
+const update = () => {
+    document.getElementById('counter').textContent = store.getState();
+}
+
+
+store.subscribe(update);
+/* store.dispatch({type: 'INC'}); */
+
